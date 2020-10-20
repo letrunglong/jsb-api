1a0749b4e44ec90886a2f7858f0c6b45fa79ffee
Long <letrunglong07@gmail.com>
2020-10-20 09:34:58 +0700

    commit on 'master'

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import '@trendmicro/react-sidenav/dist/react-sidenav.css';
+import dashboardIcon from './images/icons/dashboard.svg';
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        Home
+      </div>
+    );
+  }
+}
+class Devices extends Component {
+  render() {
+    return (
+      <div>
+        Devices
+      </div>
+    );
+  }
+}
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+    <Route render={({ location, history }) => (
+        <React.Fragment>
+            <SideNav
+                onSelect={(selected) => {
+                    const to = '/' + selected;
+                    if (location.pathname !== to) {
+                        history.push(to);
+                    }
+                }}
+            >
+                <SideNav.Nav defaultSelected="home">
+                    <NavItem eventKey="home">
+                        <NavIcon>
+                            <i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }} />
+                        </NavIcon>
+                        <NavText>
+                            Home
+                        </NavText>
+                    </NavItem>
+                    <NavItem eventKey="devices">
+                        <NavIcon>
+                            <i className="fa fa-fw fa-device" style={{ fontSize: '1.75em' }} />
+                        </NavIcon>
+                        <NavText>
+                            Devices
+                        </NavText>
+                    </NavItem>
+                </SideNav.Nav>
+            </SideNav>
+            <main>
+                <Route path="/home" component={props => <Home />} />
+                <Route path="/devices" component={props => <Devices />} />
+            </main>
+        </React.Fragment>
+    )}
+    />
+</Router>
     </div>
   );
 }
