45ddb66d39f0bad92ead69392d52632d3508bcdb
Long <letrunglong07@gmail.com>
2020-10-27 11:24:07 +0700

    done

--- src/Components/Networks.js
+++ src/Components/Networks.js
@@ -1,10 +1,65 @@
 import React, { Component } from 'react';
+import {   Image     } from 'antd'
+import copyIcon from '../images/icons/copyicon.svg';
+import homeicon from '../images/icons/home.svg';
+import members from '../images/dashboard-img/frend.svg';
+import global from '../images/dashboard-img/global.svg';
+import totalCart from '../images/dashboard-img/total.svg';
+import totalCommission from '../images/dashboard-img/pega.svg';
+import totalDirect from '../images/dashboard-img/direct.svg';
+
+class Items extends Component {
+    render() {
+        return (
+            <div className='dashboard-search'>
+                <div className='search-1'>
+                    <p className='title'>Referals link</p>
+                    <div className='link-copy'>
+                        <p className='link-text'>https://pegaswap.com/signup?sponsorkey=winwin</p>
+                        <Image src={copyIcon} />
+                    </div>
+                </div>
+                <div className='search-2'>
+                    <p className='title'>Referals code</p>
+                    <div className='link-copy text'>
+                        <p className='link-text'>winwin</p>
+                        <Image src={copyIcon} />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+class OneDashBoardContent extends Component {
+    render() {
+        return (
+            <div className={this.props.class}>
+                <div className='image-dashboard'>
+                    <Image src={this.props.src}/>
+                </div>
+                <div className='text-dashboard'>
+                    <p className='price-dashboard'>{this.props.price}</p>
+                    <p className='title-dashboard'>{this.props.title}</p>
+                </div>
+            </div>
+        );
+    }
+}
 
 class Networks extends Component {
     render() {
         return (
-            <div>
-                Networks
+            <div className='networks dashboard container'>
+                <div className='title'>Networks</div>
+                <div className='networks-content dashboard-content'>
+                    <OneDashBoardContent src={homeicon} price='Level title' class="one-items istop"/>
+                    <OneDashBoardContent src={members} price='1000' title='Members' class="one-items istop"/>
+                    <OneDashBoardContent src={global} price='$1000' title='Total networks buy' class="one-items istop"/>
+                    <OneDashBoardContent src={totalCart} price='$1000' title='Total direct buy' class="one-items"/>
+                    <OneDashBoardContent src={totalCommission} price='$1000' title='Total commission' class="one-items"/>
+                    <OneDashBoardContent src={totalDirect} price='$1000' title='Direct commitsion' class="one-items"/>
+                </div>
+                <Items/>
             </div>
         );
     }
--- src/Components/Settings.js
+++ src/Components/Settings.js
@@ -1,13 +1,134 @@
 import React, { Component } from 'react';
+import { Button, Input, Tabs } from 'antd'
+import person from '../images/settings-img/personnal.svg'
+import authen from '../images/settings-img/authen.svg'
+import secu from '../images/settings-img/security.svg'
+import twof from '../images/settings-img/twofactor.svg'
+import Search from 'antd/lib/input/Search';
+class LastElement extends Component {
+  render() {
+    return (
+      <div className='parent-flex'>
+        <div className='child-1'>
+          <p>{this.props.firstText}</p>
+          <Input defaultValue={this.props.firstInput} />
+        </div>
+        <div className='child-2'>
+          <p>{this.props.secondText}</p>
+          <Button className='btn-save'>Save Now!</Button>
+        </div>
+      </div>
+    );
+  }
+}
+
+class ItemsPersonalTab extends Component {
+  render() {
+    return (
+      <div className='parent-flex'>
+        <div className='child-1'>
+          <p>{this.props.firstText}</p>
+          <Input defaultValue={this.props.firstInput} />
+        </div>
+        <div className='child-2'>
+          <p>{this.props.secondText}</p>
+          <Input defaultValue={this.props.secondInput} />
+        </div>
+      </div>
+    );
+  }
+}
+
+class TabPersonal extends Component {
+  render() {
+    return (
+      <div className='personal-content tab-content'>
+        <ItemsPersonalTab firstText='First Name' secondText='Last Name' firstInput='David' secondInput='David' />
+        <ItemsPersonalTab firstText='Birth day' secondText='Gender' firstInput='20/12/2020' secondInput='Male' />
+        <ItemsPersonalTab firstText='Address' secondText='Country' firstInput='123 Abcd' secondInput='USA' />
+        <LastElement firstText='Phone Number' firstInput='123 4567' />
+      </div>
+    );
+  }
+}
+class TabAuthen extends Component {
+  render() {
+    return (
+      <div className='authen-content tab-content'>
+        <div className='title'>
+          <div>Change Password</div>
+          <div>Change to another email</div>
+        </div>
+        <div className='content'>
+          {/* 1 */}
+          <div className='one-content'>
+            <div className='child-1'>
+              <p>Current password</p>
+              <Input defaultValue='asdasdasd' type='password' />
+            </div>
+            <div className='child-2'>
+              <p>New email</p>
+              <Search
+                defaultValue="new-email@gmail.com"
+                allowClear
+                enterButton="Change"
+              />
+            </div>
+          </div>
+           {/* 2 */}
+          <div className='one-content'>
+            <div className='child-1'>
+              <p>New password</p>
+              <Input defaultValue='asdasdasd' type='password' />
+            </div>
+            <div className='child-2'>
+            </div>
+          </div>
+          {/* 3 */}
+          <div className='one-content'>
+            <div className='child-1'>
+              <p>Confirm password</p>
+              <Input defaultValue='asdasdasd' type='password' />
+            </div>
+            <div className='child-2'>
+              <Button className='btn-save'>Save now!</Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+
+const { TabPane } = Tabs;
+const Tab = () => (
+  <Tabs defaultActiveKey="1">
+    <TabPane tab="Personal" key="1">
+      <TabPersonal />
+    </TabPane>
+    <TabPane tab="Authentication" key="2">
+      <TabAuthen />
+    </TabPane>
+    <TabPane tab="Two-factor" key="3">
+      Content of Tab Pane 3
+      </TabPane>
+    <TabPane tab="Security" key="4">
+      Content of Tab Pane 4
+      </TabPane>
+  </Tabs>
+);
 
 class Settings extends Component {
-    render() {
-        return (
-            <div>
-                Settings
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className='settings container'>
+        <div className='settings-content'>
+          <Tab />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Settings;
